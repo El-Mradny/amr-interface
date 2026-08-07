27e318dcbf8454c4bcc7052def85f46d6b48c221
Ahmed Ismail <76683876+El-Mradny@users.noreply.github.com>
2026-08-07 20:24:58 +0300

    Update import path for PrismaClient

--- prisma.ts
+++ prisma.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from "@/app/generated/prisma/client";
+import {PrismaClient} from "@/generated/prisma/client";
 import {PrismaMariaDb} from "@prisma/adapter-mariadb";
 
 const globalForPrisma = global as unknown as {
